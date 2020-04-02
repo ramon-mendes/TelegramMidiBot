ca5e95c71a28f86325c147e3f23438dc60e7b7b1
Ramon Mendes <ramon@misoftware.com.br>
2020-04-02 09:44:47 -0300

    g

--- TelegramMidiBot/Startup.cs
+++ TelegramMidiBot/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace TelegramMidiBot
 {
@@ -52,6 +55,39 @@ namespace TelegramMidiBot
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
+
+			StartBot();
+		}
+
+		static ITelegramBotClient botClient;
+
+		static void StartBot()
+		{
+			Task.Run(() =>
+			{
+				var botClient = new TelegramBotClient("925635831:AAEkK38Samii6HNYUAeS3EfJZrk81qlG6Ks");
+				var me = botClient.GetMeAsync().Result;
+				Console.WriteLine(
+				  $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+				);
+
+				botClient.OnMessage += Bot_OnMessage;
+				botClient.StartReceiving();
+				Thread.Sleep(int.MaxValue);
+			});
+		}
+
+		static async void Bot_OnMessage(object sender, MessageEventArgs e)
+		{
+			if(e.Message.Text != null)
+			{
+				Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+				await botClient.SendTextMessageAsync(
+				  chatId: e.Message.Chat,
+				  text: "You said:\n" + e.Message.Text
+				);
+			}
 		}
 	}
 }
